72a76f7fbc804987d67be31649a4bdc07c665e30
emddevelop <stadio18@hotmail.com>
2017-06-09 15:30:57 +0300

    new files

--- controllers/index.js
+++ controllers/index.js
@@ -1,3 +1,14 @@
-/**
- * Created by Marina on 8/6/2017.
- */
+var express=require('express');
+var router=express.Router();
+
+//Home page
+router.get('/',function(req,res){
+    res.render('index');
+})
+
+//Home page
+router.get('/login',function(req,res){
+    res.render('login');
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,3 +1,33 @@
-/**
- * Created by Marina on 8/6/2017.
- */
+var express=require('express');
+var path=require('path');
+var bodyParser = require('body-parser');
+var validate = require('express-validator');
+
+var router=express.Router();
+var server=express();
+
+//view engine setup
+server.set('views', path.join(__dirname, 'views'));
+server.set('view engine', 'ejs'); // set up ejs for templating
+
+//middlewares
+//server.use(require('./middlewares/users'));
+app.use(expressValidator({
+    customValidators: {
+        isArray: function(value) {
+            return Array.isArray(value);
+        },
+        gte: function(param, num) {
+            return param >= num;
+        }
+    }
+}));
+server.use(require('./controllers'));
+/*We load the controllers/index.js file.
+Moreover, it is an index file, so we don’t
+need to provide its name when requiring it,
+we only need the folder name.*/
+
+server.listen(6969,function(){
+    console.log('Server started....');
+})
